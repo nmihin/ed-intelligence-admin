c8e1570835999401496e8c7bb4103449b21c0fc1
nmihin <nmihin@gmail.com>
2020-12-22 19:52:30 +0100

    - added table and connected records and serach for student ranking

--- src/pages/SchoolReports/StudentRank.vue
+++ src/pages/SchoolReports/StudentRank.vue
@@ -11,24 +11,34 @@
         </div>
       </div>
       <!-- LIST VIEW -->
-      <el-table ref="singleTable" stripe :data="posts" highlight-current-row style="width: 100%">
-        <el-table-column sortable property="sn" label="SN" width="80"></el-table-column>
-        <el-table-column sortable property="code" width="80" label="Code"></el-table-column>
-        <el-table-column sortable property="category" width="120" label="Category"></el-table-column>
-        <el-table-column sortable property="codename" label="Codename"></el-table-column>
-        <el-table-column sortable property="usageguidelines" label="Usage Guidelines"></el-table-column>
-        <el-table-column sortable property="additionalguidelines" label="Additional Guidelines"></el-table-column>
-        <el-table-column width="140" property="action" label="Action">
-          <div slot-scope="scope">
-            <div @click="editSelectedCode(scope.row.sn);" class="element-edit">
-              <i class="icon icon-edit"></i>
-            </div>
-            <div @click="deleteSelectedCode(scope.row.sn);" class="element-delete">
-              <i class="icon icon-delete"></i>
-            </div>
-          </div>
-        </el-table-column>
-      </el-table>
+      <div class="row">
+        <div class="col-12">
+          <el-table ref="singleTable" stripe :data="posts" highlight-current-row style="width: 100%">
+            <el-table-column sortable property="sn" label="SN" width="80"></el-table-column>
+            <el-table-column sortable property="usi" width="80" label="USI"></el-table-column>
+            <el-table-column sortable property="name" width="120" label="Name"></el-table-column>
+            <el-table-column sortable property="surname" label="Surname"></el-table-column>
+            <el-table-column sortable property="grade" label="Grade"></el-table-column>
+            <el-table-column sortable property="elaSGPRank" label="ELA SGP Rank"></el-table-column>
+            <el-table-column sortable property="mathSGPRank" label="Math SGP Rank"></el-table-column>
+            <el-table-column sortable property="elaScaledScoreRank" label="ELA Scaled Score Ranke"></el-table-column>
+            <el-table-column sortable property="mathScaledScoreRank" label="Math Scaled Score Ranke"></el-table-column>
+            <el-table-column sortable property="elaLevel3" label="ELA (level 3+)"></el-table-column>
+            <el-table-column sortable property="mathLevel3" label="Math (level 3+)"></el-table-column>
+            <el-table-column sortable property="elaLevel4" label="ELA (level 4+)"></el-table-column>
+            <el-table-column sortable property="mathLevel4" label="Math (level 4+)"></el-table-column>
+            <el-table-column sortable property="reEnrolled" label="Re-Enrolled"></el-table-column>
+            <el-table-column sortable property="inSeatAttendanceRank" label="In-Seat Attendance Rank"></el-table-column>
+            <el-table-column sortable property="finalRank" label="Final Rank"></el-table-column>
+          </el-table>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <el-pagination background layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="pageSize" :total="totalSize">
+          </el-pagination>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,45 +56,107 @@
     // DATA
     data: () => ({
       posts: [],
+      limit: 10,
       page: 1,
       pageSize: 10,
       totalSize: 0
     }),
     methods: {
-      searchFilter(searchName) {
-        this.searchFilterParent(searchName);
-      },
-      loadMore() {
-        const codeBookStorage = localStorage.getItem("codeBookStorageJSONData")
+       loadMore() {
+         this.busy = true;
+         const studentRankStorage = this.loadStudentRankStorage();
+
+         if (studentRankStorage) {
+
+            const append = studentRankStorage.slice(
+              this.posts.length,
+              this.posts.length + this.limit
+            );
+
+            this.posts = append;
+
+           this.busy = false;
+         } else {
+           this.axios.get("https://raw.githubusercontent.com/nmihin/ed-intelligence-admin/main/public/student-rank.json").then((response) => {
+
+            const append = response.data.slice(
+              this.posts.length,
+              this.posts.length + this.limit
+            );
+
+             this.posts = append;
+
+             localStorage.setItem("studentRankStorageJSONData",JSON.stringify(response.data));
+             this.busy = false;
+           }).catch((error) => error.response.data)
+         }
+       },
+      updatePagination(value) {
+        this.pageSize = value;
+
+        const studentRankStorage = this.loadStudentRankStorage();
+
         this.posts = [];
+        const append = studentRankStorage.slice(
+          this.posts.length,
+          this.posts.length + this.pageSize
+        );
 
-        if (codeBookStorage) {
-          this.totalSize = this.parentData[1].exit.length;
+        this.posts = append;
+      },
+      searchFilter(value) {
+        this.busy = true;
 
-          const append = this.parentData[1].exit.slice(
-            this.posts.length,
-            this.posts.length + this.pageSize
+        const studentRankStorage = this.loadStudentRankStorage();
+
+        this.posts = studentRankStorage.filter((data) =>
+          data.name.toLowerCase().includes(value.toLowerCase())
+        );
+
+        this.totalSize = studentRankStorage.length;
+
+        this.busy = false;
+        return studentRankStorage;
+      },
+       handleCurrentChange(val) {
+        this.busy = true;
+        const studentRankStorage = this.loadStudentRankStorage();
+
+        this.page = val;
+
+        // CHECK IF SEARCH EMPTY
+        if (this.searchName === "") {
+          this.totalSize = studentRankStorage.length;
+
+          const append = studentRankStorage.slice(
+            (this.page - 1) * this.pageSize,
+            (this.page - 1) * this.pageSize + this.pageSize
           );
 
           this.posts = append;
         } else {
-          setTimeout(() => {
-            this.totalSize = this.parentData[1].exit.length;
 
-            const append = this.parentData[1].exit.slice(
-              this.posts.length,
-              this.posts.length + this.pageSize
-            );
+          this.posts = studentRankStorage.filter((data) =>
+            data.name.toLowerCase().includes(this.searchName.toLowerCase())
+          );
 
-            this.posts = append;
-          }, 1000)
+          this.totalSize = this.posts.length;
+
+          const append = this.posts.slice(
+            (this.page - 1) * this.pageSize,
+            (this.page - 1) * this.pageSize + this.pageSize
+          );
+
+          this.posts = append;
         }
-      },
-      // LOCALSTORAGE
-      loadCodeBookStorage() {
-        return JSON.parse(localStorage.getItem("codeBookStorageJSONData"));
-      },
-    },
+
+        this.busy = false;
+       },
+       // LOCALSTORAGE
+       loadStudentRankStorage() {
+         return JSON.parse(localStorage.getItem("studentRankStorageJSONData"));
+       }
+     },
     created() {
       this.loadMore();
     }
