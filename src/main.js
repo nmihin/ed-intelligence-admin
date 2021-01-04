80e41674277d562c45869e0e457c0d7fcdab1a4e
nmihin <nmihin@gmail.com>
2021-01-04 12:52:51 +0100

    - organising styles into seprate files

--- src/main.js
+++ src/main.js
@@ -6,13 +6,8 @@ import $ from 'jquery';
 import Notifications from 'vue-notification';
 import VueLazyload from 'vue-lazyload';
 import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.min.css';
 
 import './assets/css/styles.css';
-import './assets/css/swiper.min.css';
-import './assets/css/bootstrap-grid.min.css';
-import './assets/css/ui-elements.css';
-import './assets/css/bootstrap.min.css';
 
 Vue.use(Notifications);
 Vue.use(VueLazyload);
