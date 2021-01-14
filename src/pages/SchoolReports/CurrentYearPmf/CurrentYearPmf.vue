0cccd18aca73a6c5b8f453987b4100e33a1b7e2e
nmihin <nmihin@gmail.com>
2021-01-14 15:13:22 +0100

    - added chart component for pmf reports and connected with data

--- src/pages/SchoolReports/CurrentYearPmf/CurrentYearPmf.vue
+++ src/pages/SchoolReports/CurrentYearPmf/CurrentYearPmf.vue
@@ -9,9 +9,18 @@
           <div class="row">
             <div class="report-table-header col-12">{{studentProgressData[0].report}}</div>
             <el-table stripe ref="singleTable" :data="studentProgressData[0].data" highlight-current-row style="width: 100%">
-              <el-table-column property="studentProgress" label="Student Progress"></el-table-column>
+              <el-table-column property="studentProgress" width="200" label="Student Progress"></el-table-column>
               <el-table-column property="rate" label="Rate"></el-table-column>
               <el-table-column property="floor" label="Floor"></el-table-column>
+              <el-table-column width="200" property="chartData">
+              <template v-slot="scope">
+                <!-- Available Colors (red,blue,green) -->
+                <CurrentYearPmfChart 
+                  :chartPercentageParent="scope.row.chartData"
+                  :chartColorParent="'red'"
+                />
+              </template>
+              </el-table-column>
               <el-table-column property="target" label="Target"></el-table-column>
               <el-table-column property="possible" label="Possible"></el-table-column>
               <el-table-column property="possiblePoints" label="Possible points"></el-table-column>
@@ -24,9 +33,18 @@
             <!-- DATASET 1 -->
             <div class="report-table-header col-12">{{studentAchievementsData[0].report}}</div>
             <el-table stripe ref="singleTable" :data="studentAchievementsData[0].data" highlight-current-row style="width: 100%">
-              <el-table-column property="studentAchievements" label="Student Achievements"></el-table-column>
+              <el-table-column property="studentAchievements"  width="200" label="Student Achievements"></el-table-column>
               <el-table-column property="rate" label="Rate"></el-table-column>
               <el-table-column property="floor" label="Floor"></el-table-column>
+              <el-table-column width="200" property="chartData">
+              <template v-slot="scope">
+                <!-- Available Colors (red,blue,green) -->
+                <CurrentYearPmfChart 
+                  :chartPercentageParent="scope.row.chartData"
+                  :chartColorParent="'blue'"
+                />
+              </template>
+              </el-table-column>
               <el-table-column property="target" label="Target"></el-table-column>
               <el-table-column property="possible" label="Possible"></el-table-column>
               <el-table-column property="possiblePoints" label="Possible points"></el-table-column>
@@ -34,9 +52,18 @@
             <!-- DATASET 2 -->
             <div class="report-table-header col-12">{{studentAchievementsData[1].report}}</div>
             <el-table stripe ref="singleTable" :data="studentAchievementsData[1].data" highlight-current-row style="width: 100%">
-              <el-table-column property="studentAchievements" label="Student Achievements"></el-table-column>
+              <el-table-column property="studentAchievements" width="200" label="Student Achievements"></el-table-column>
               <el-table-column property="rate" label="Rate"></el-table-column>
               <el-table-column property="floor" label="Floor"></el-table-column>
+              <el-table-column width="200" property="chartData">
+              <template v-slot="scope">
+                <!-- Available Colors (red,blue,green) -->
+                <CurrentYearPmfChart 
+                  :chartPercentageParent="scope.row.chartData"
+                  :chartColorParent="'blue'"
+                />
+              </template>
+              </el-table-column>
               <el-table-column property="target" label="Target"></el-table-column>
               <el-table-column property="possible" label="Possible"></el-table-column>
               <el-table-column property="possiblePoints" label="Possible points"></el-table-column>
@@ -47,9 +74,18 @@
           <div class="student-report-header col-12">Gateway</div>
           <div class="row">
             <el-table stripe ref="singleTable" :data="gatewayData" highlight-current-row style="width: 100%">
-              <el-table-column property="gateway" label="Gateway"></el-table-column>
+              <el-table-column property="gateway" width="200" label="Gateway"></el-table-column>
               <el-table-column property="rate" label="Rate"></el-table-column>
               <el-table-column property="floor" label="Floor"></el-table-column>
+              <el-table-column width="200" property="chartData">
+              <template v-slot="scope">
+                <!-- Available Colors (red,blue,green) -->
+                <CurrentYearPmfChart 
+                  :chartPercentageParent="scope.row.chartData"
+                  :chartColorParent="'blue'"
+                />
+              </template>
+              </el-table-column>
               <el-table-column property="target" label="Target"></el-table-column>
               <el-table-column property="possible" label="Possible"></el-table-column>
               <el-table-column property="possiblePoints" label="Possible points"></el-table-column>
@@ -60,9 +96,18 @@
           <div class="student-report-header">School Environment</div>
           <div class="row">
             <el-table stripe ref="singleTable" :data="schoolEnvironmentData" highlight-current-row style="width: 100%">
-              <el-table-column property="schoolEnvironment" label="School Environment"></el-table-column>
+              <el-table-column property="schoolEnvironment"  width="200" label="School Environment"></el-table-column>
               <el-table-column property="rate" label="Rate"></el-table-column>
               <el-table-column property="floor" label="Floor"></el-table-column>
+              <el-table-column width="200" property="chartData">
+              <template v-slot="scope">
+                <!-- Available Colors (red,blue,green) -->
+                <CurrentYearPmfChart 
+                  :chartPercentageParent="scope.row.chartData"
+                  :chartColorParent="'red'"
+                />
+              </template>
+              </el-table-column>
               <el-table-column property="target" label="Target"></el-table-column>
               <el-table-column property="possible" label="Possible"></el-table-column>
               <el-table-column property="possiblePoints" label="Possible points"></el-table-column>
@@ -79,10 +124,12 @@
 </template>
 
 <script>
+  import CurrentYearPmfChart from './charts/CurrentYearPmfChart'
+
   export default {
     name: "current-year-pmf",
     components: {
-
+      CurrentYearPmfChart
     },
     // DATA
     data: () => ({
@@ -92,6 +139,7 @@
               "studentProgress": "Growth on the state assessments in reading over time",
               "rate": 53.0,
               "floor": 30.0,
+              "chartData": 25,
               "target": 100.0,
               "possible": "5.8 out of 17.5",
               "possiblePoints": 32.9
@@ -100,6 +148,7 @@
               "studentProgress": "Growth on the state assessments in reading over time",
               "rate": 42.5,
               "floor": 30.0,
+              "chartData": 45,
               "target": 70.0,
               "possible": "5.5 out of 17.5",
               "possiblePoints": 31.2
@@ -112,6 +161,7 @@
                 "studentAchievements": "Level 3+",
                 "rate": 68.0,
                 "floor": 0.0,
+                "chartData": 15,
                 "target": 100.0,
                 "possible": "5.1 out of 7.5",
                 "possiblePoints": 68.0
@@ -120,6 +170,7 @@
                 "studentAchievements": "Level 4+",
                 "rate": 57.0,
                 "floor": 0.0,
+                "chartData": 65,
                 "target": 70.0,
                 "possible": "4.1 out of 7.5",
                 "possiblePoints": 81.6
@@ -132,6 +183,7 @@
                 "studentAchievements": "Level 3+",
                 "rate": 68.0,
                 "floor": 0.0,
+                "chartData": 36,
                 "target": 100.0,
                 "possible": "5.1 out of 7.5",
                 "possiblePoints": 68.0
@@ -140,6 +192,7 @@
                 "studentAchievements": "Level 4+",
                 "rate": 57.0,
                 "floor": 0.0,
+                "chartData": 5,
                 "target": 70.0,
                 "possible": "4.1 out of 7.5",
                 "possiblePoints": 81.6
@@ -151,6 +204,7 @@
             "gateway": "3rd Grade Reading - College and Career Ready (2 year cohort)",
             "rate": 0.0,
             "floor": 50.0,
+            "chartData": 25,
             "target": 100.0,
             "possible": "5.1 out of 7.5",
             "possiblePoints": 68.0
@@ -159,6 +213,7 @@
             "gateway": "8th Grade mathematics - College and Career Ready (2 year cohort)",
             "rate": 57.0,
             "floor": 0.0,
+            "chartData": 55,
             "target": 70.0,
             "possible": "4.1 out of 7.5",
             "possiblePoints": 81.6
@@ -168,6 +223,7 @@
             "schoolEnvironment": "In Seat Attendance Rate",
             "rate": 0.0,
             "floor": 50.0,
+            "chartData": 25,
             "target": 100.0,
             "possible": "5.1 out of 7.5",
             "possiblePoints": 68.0
@@ -176,6 +232,7 @@
             "schoolEnvironment": "Re-Enrollment Rate",
             "rate": 57.0,
             "floor": 0.0,
+            "chartData": 75,
             "target": 70.0,
             "possible": "4.1 out of 7.5",
             "possiblePoints": 81.6
@@ -184,6 +241,7 @@
             "schoolEnvironment": "CLASS: Emotional Support",
             "rate": 0.0,
             "floor": 50.0,
+            "chartData": 25,
             "target": 100.0,
             "possible": "5.1 out of 7.5",
             "possiblePoints": 68.0
@@ -192,6 +250,7 @@
             "schoolEnvironment": "CLASS: Classroom Management",
             "rate": 57.0,
             "floor": 0.0,
+            "chartData": 15,
             "target": 70.0,
             "possible": "4.1 out of 7.5",
             "possiblePoints": 81.6
@@ -200,6 +259,7 @@
             "schoolEnvironment": "CLASS: Instructional Support",
             "rate": 57.0,
             "floor": 0.0,
+            "chartData": 75,
             "target": 70.0,
             "possible": "4.1 out of 7.5",
             "possiblePoints": 81.6
---
+++ src/pages/SchoolReports/CurrentYearPmf/charts/CurrentYearPmfChart.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="skills">
+    <ul class="lines">
+      <li class="line l--0">
+        <span class="line__label">
+          0%
+        </span>
+      </li>
+      <li class="line l--25">
+        <span class="line__label">
+          25%
+        </span>
+      </li>
+      <li class="line l--50">
+        <span class="line__label">
+          50%
+        </span>
+      </li>
+      <li class="line l--75">
+        <span class="line__label">
+          75%
+        </span>
+      </li>
+      <li class="line l--100">
+        <span class="line__label">
+          100%
+        </span>
+      </li>
+    </ul>
+
+    <div class="charts">
+      <div class="chart chart--dev">
+        <ul class="chart--horiz">
+          <li class="chart__bar" :class="color" :style="{'width':percentage+'%'}">
+            <span class="chart__label">
+                {{percentage}}%
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "current-year-pmf",
+    components: {},
+    // DATA
+    data: () => ({
+        percentage:0,
+    }),
+    props: {
+        chartPercentageParent:Number,
+        chartColorParent:String
+    },
+    methods: {
+        loadMore(){
+            this.percentage = this.chartPercentageParent;
+            this.color = this.chartColorParent;
+        }
+    },
+    created() {
+      this.loadMore();
+    }
+  }
+
+</script>
