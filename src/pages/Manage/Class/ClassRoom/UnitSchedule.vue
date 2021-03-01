2921ea2fca31df4924569f7a28de1561743dc161
nmihin <nmihin@gmail.com>
2021-03-01 08:22:02 +0100

    - added popover component for events - added IDs for events with descriptions

--- src/pages/Manage/Class/ClassRoom/UnitSchedule.vue
+++ src/pages/Manage/Class/ClassRoom/UnitSchedule.vue
@@ -41,8 +41,18 @@
            student:'',
            selectedDate:'',
            events: [
-              { title: 'Event 1', date: '2021-02-12' },
-              { title: 'Event 2', date: '2021-02-22' }
+              { 
+                id:1,
+                title: 'Event 1111', 
+                date: '2021-03-02',
+                description: 'This is a cool event'
+              },
+              { 
+                id:2,
+                title: 'Event 2222', 
+                date: '2021-03-22', 
+                description: 'This is a cool event'
+              }
            ]
         }),
         methods: {
@@ -56,10 +66,15 @@
               this.$refs.openUnitModal.openModal(this.subject,this.grade,this.student,date);
             },
             eventAddedChild(eventData){
+              //MAX ID
+              const idx = Math.max(...this.events.map(el => el.id));
+
               this.events.push(
                 {
+                  id: idx+1,
                   title: eventData[0] +' '+ eventData[1],
-                  date: this.selectedDate.dateStr
+                  date: this.selectedDate.dateStr,
+                  description: "Event text..."
                 }
               )
             }
